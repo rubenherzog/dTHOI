a5d330110abf695e2b5c786a19e64a1b3621186e
Rubén Herzog <rubenherzog@users.noreply.github.com>
2026-08-27 21:33:26 +0200

    Simplify top-level public API

--- dthoi/__init__.py
+++ dthoi/__init__.py
@@ -1,26 +1,21 @@
 """dTHOI: higher-order information analysis for discrete multivariate data."""
 
-from .batch import multi_order_measures
-from .data import PreparedDiscreteData, prepare_discrete_data
-from .entropy.base import CountingEntropyProvider
-from .entropy.cache import EntropyCache
-from .entropy.estimators import (
-    CountEntropyEstimator,
-    MillerMadowEstimator,
-    PluginEstimator,
-    resolve_estimator,
+from .api import (
+    DiscreteData,
+    InformationMeasures,
+    InteractionResults,
+    analyze_orders,
+    entropy,
+    information_measures,
+    prepare_data,
 )
-from .measures.core import nplets_measures
 
 __all__ = [
-    "CountEntropyEstimator",
-    "CountingEntropyProvider",
-    "EntropyCache",
-    "MillerMadowEstimator",
-    "PluginEstimator",
-    "PreparedDiscreteData",
-    "multi_order_measures",
-    "nplets_measures",
-    "prepare_discrete_data",
-    "resolve_estimator",
+    "DiscreteData",
+    "InformationMeasures",
+    "InteractionResults",
+    "analyze_orders",
+    "entropy",
+    "information_measures",
+    "prepare_data",
 ]
